4e12fd758553cbd330a2ec6e2b941bdd7b131776
Budi Santoso <budi.santoso290103@gmail.com>
2026-05-21 17:57:46 +0700

    migration log

---
+++ database/migrations/2026_05_20_025040_create_login_logs_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('login_logs', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedBigInteger('user_id');
+
+            $table->string('ip_address', 45)->nullable();
+
+            $table->string('device')->nullable();
+
+            $table->string('browser')->nullable();
+
+            $table->timestamp('login_time');
+
+            $table->timestamp('logout_time')->nullable();
+
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('login_logs');
+    }
+};
