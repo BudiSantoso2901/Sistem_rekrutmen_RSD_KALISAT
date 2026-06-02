ac0d06c6b0132aea4fbad9d0bfa30c2ec877c734
Budi Santoso <budi.santoso290103@gmail.com>
2026-06-03 00:01:44 +0700

    balik route

--- routes/web.php
+++ routes/web.php
@@ -19,12 +19,12 @@ use Illuminate\Support\Facades\Response;
 |
 */
 
-Route::get('/', function () {
-    return view('coming');
-});
+// Route::get('/', function () {
+//     return view('coming');
+// });
 
-// Route::get('/', [PelamarController::class, 'tampil_halaman_pelamar'])->name('pelamar.form');
-// Route::post('/pelamar/store', [PelamarController::class, 'store'])->name('pelamar.store');
+Route::get('/', [PelamarController::class, 'tampil_halaman_pelamar'])->name('pelamar.form');
+Route::post('/pelamar/store', [PelamarController::class, 'store'])->name('pelamar.store');
 Route::get('/pelamar/hasil/{token}', [PelamarController::class, 'hasil'])
     ->name('Pelamar.hasil');
 
